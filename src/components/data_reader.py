1be9769feaf132645f04ed595626f57d2a98f620
jooaobrum <git>
2025-02-02 16:39:54 +0100

    adding data reader class and tests

---
+++ src/components/data_reader.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from components.config import ProjectConfig
+
+class DataReader:
+    """
+    A class for reading CSV files using pandas.
+    
+    Attributes:
+    -----------
+    filename : str
+        The path to the CSV file.
+    
+    Methods:
+    --------
+    read_csv() -> pd.DataFrame
+        Reads the CSV file and returns it as a pandas DataFrame.
+    """
+    
+    def __init__(self, config: ProjectConfig):
+        """
+        Initializes the DataReader with the specified CSV filename.
+        
+        Parameters:
+        -----------
+        filename : str
+            The path to the CSV file.
+        """
+        self.input_data = config.input_data
+    
+    def read_csv(self) -> pd.DataFrame:
+        """
+        Reads the CSV file and returns its contents as a pandas DataFrame.
+        
+        Returns:
+        --------
+        pd.DataFrame
+            A DataFrame containing the data from the CSV file.
+        """
+        return pd.read_csv(self.input_data)

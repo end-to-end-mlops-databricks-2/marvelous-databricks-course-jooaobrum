5491d147ba4e4021d38e681ae74255810a52373e
jooaobrum <git>
2025-02-16 18:04:09 +0100

    adding notebook as source

--- scripts/03_deploy_fe_model_serving_endpoint.py
+++ scripts/03_deploy_fe_model_serving_endpoint.py
@@ -1,8 +1,11 @@
+# Databricks notebook source
+
 from loguru import logger
 
 from components.config import ProjectConfig
 from components.serving.fe_model_serving import FeatureLookupServing
 
+# COMMAND ----------
 # Load project config
 config = ProjectConfig.from_yaml(config_path="../project_config.yml")
 logger.info("Loaded config file.")
@@ -11,6 +14,7 @@ catalog_name = config.catalog_name
 schema_name = config.schema_name
 endpoint_name = "hotel-reservation-model-serving-fe"
 
+# COMMAND ----------
 # Initialize Feature Lookup Serving Manager
 feature_model_server = FeatureLookupServing(
     model_name="hotel_reservation_model_fe",
@@ -18,10 +22,12 @@ feature_model_server = FeatureLookupServing(
     feature_table_name=f"{catalog_name}.{schema_name}.hotel_reservation_features",
 )
 
+# COMMAND ----------
 # Create the online table for house features
 feature_model_server.create_online_table()
 logger.info("Created online table")
 
+# COMMAND ----------
 # Deploy the model serving endpoint with feature lookup
 feature_model_server.deploy_or_update_serving_endpoint()
 logger.info("Started deployment/update of the serving endpoint")

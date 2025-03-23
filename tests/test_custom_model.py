ea65b1dc1c322ca65bb1f03d5f13dc987e8e2c6b
jooaobrum <git>
2025-03-23 15:12:59 +0100

    refactoring tests

--- tests/test_custom_model.py
+++ tests/test_custom_model.py
@@ -9,21 +9,34 @@ from components.models.custom_model import CustomModel  # Replace 'your_script' 
 def mock_config():
     return ProjectConfig(
         input_data="dummy_path.csv",
-        num_features=["num_feature1", "num_feature2"],
-        cat_features=["cat_feature1", "cat_feature2"],
-        target="booking_status",
+        num_features=[
+            {"name": "num_feature1", "alias": "num1", "dtype": "int32"},
+            {"name": "num_feature2", "alias": "num2", "dtype": "int32"}
+        ],
+        cat_features=[
+            {"name": "cat_feature1", "alias": "cat_feature1", "dtype": "category"},
+            {"name": "cat_feature2", "alias": "cat_feature2", "dtype": "category"}
+        ],
+        target={
+            "name": "booking_status",
+            "alias": "booking_status",
+            "dtype": "int32",
+            "mapping": {"Canceled": 1, "Not_Canceled": 0}
+        },
         test_size=0.2,
         random_state=42,
         parameters={"n_estimators": 100},
-        catalog_name="test_catalog",
-        schema_name="test_schema",
-        experiment_name="test_experiment",
+        endpoint_name="test-endpoint",
+        id_columns=["id"],
+        catalog_name="uc_test",
+        schema_name="hotel_reservation",
+        experiment_name="hotel_booking_test"
     )
 
 
 @pytest.fixture
 def mock_tags():
-    return Tags(git_sha="123", branch="test")
+    return Tags(git_sha="123", branch="test", job_run_id = "test")
 
 
 @pytest.fixture
@@ -35,9 +48,9 @@ def mock_model(mock_config, mock_tags):
 def fake_data():
     return pd.DataFrame(
         {
-            "booking_status": ["Not_Canceled", "Canceled", "Not_Canceled"],
-            "num_feature1": ["10", "20", "30"],
-            "num_feature2": ["100", "200", "300"],
+            "booking_status": [0, 1, 0],
+            "num1": ["10", "20", "30"],
+            "num2": ["100", "200", "300"],
             "cat_feature1": ["A", "B", "C"],
             "cat_feature2": [1, 2, 3],  # Intentionally numeric to test conversion
         }
@@ -48,12 +61,15 @@ def test_load_data(mock_model, fake_data):
     mock_model.train_set = fake_data.copy()
     mock_model.test_set = fake_data.copy()
 
-    mock_model.X_train = mock_model.train_set[mock_model.config.num_features + mock_model.config.cat_features]
-    mock_model.y_train = mock_model.train_set[mock_model.config.target]
+    features = [feat['alias'] for feat in mock_model.config.num_features + mock_model.config.cat_features]
+    target = mock_model.config.target['alias']
+
+    mock_model.X_train = mock_model.train_set[features]
+    mock_model.y_train = mock_model.train_set[target]
 
     assert not mock_model.train_set.empty
-    assert "num_feature1" in mock_model.X_train.columns
-    assert "num_feature2" in mock_model.X_train.columns
+    assert "num1" in mock_model.X_train.columns
+    assert "num2" in mock_model.X_train.columns
     assert "cat_feature1" in mock_model.X_train.columns
     assert "cat_feature2" in mock_model.X_train.columns
     assert "booking_status" in mock_model.y_train.name
--- tests/test_project_cfg.py
+++ tests/test_project_cfg.py
@@ -14,7 +14,7 @@ def test_yaml_file_valid():
     config_path = "../project_config.yml"
 
     try:
-        config = ProjectConfig.from_yaml(config_path)
+        config = ProjectConfig.from_yaml(config_path, env = "dev")
     except ValidationError as e:
         raise RuntimeError(f"Error loading {config_path}") from e
 
@@ -22,11 +22,9 @@ def test_yaml_file_valid():
     assert isinstance(config.test_size, float)
     assert isinstance(config.random_state, int)
     assert isinstance(config.num_features, list)
-    assert all(isinstance(feat, str) for feat in config.num_features)
+    assert all(isinstance(feat, dict) for feat in config.num_features)
     assert isinstance(config.cat_features, list)
-    assert all(isinstance(feat, str) for feat in config.cat_features)
-    assert isinstance(config.target, str)
-    assert isinstance(config.catalog_name, str)
-    assert isinstance(config.schema_name, str)
+    assert all(isinstance(feat, dict) for feat in config.cat_features)
+    assert isinstance(config.target, dict)
     assert isinstance(config.experiment_name, str)
     assert isinstance(config.parameters, dict)
